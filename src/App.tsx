592eb470db026fffac63b2f2913e780c4be60117
z1edsh1ro <ziedshero@gmail.com>
2025-04-05 19:40:33 +0700

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ShopProvider } from "./context/ShopContext";
@@ -13,9 +10,6 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
       <ShopProvider>
         <BrowserRouter>
           <Routes>
@@ -26,7 +20,6 @@ const App = () => (
           </Routes>
         </BrowserRouter>
       </ShopProvider>
-    </TooltipProvider>
   </QueryClientProvider>
 );
 
